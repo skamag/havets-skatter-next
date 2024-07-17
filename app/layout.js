a55ebd2a606cd66e88b3c32be82bb08165a2bbe7
unknown <Fundri@UL04.funkweb.lan>
2024-07-17 13:10:16 +0200

    logo and layout

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,16 @@
 import Navigation from "./components/Navigation"
+import Logo from "./components/Logo"
+
+export const metadata = {
+  title: "The Wild Oasis",
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Navigation />
+        <Logo />
         <main>{children}</main>
         <footer>Copyright by The Wild Oasis</footer>
       </body>
