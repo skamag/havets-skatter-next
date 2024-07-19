a94d2945d014094e8bb5eec2bee6166ae32f699a
unknown <magneskaro@gmail.com>
2024-07-19 11:49:55 +0200

    apos 2nd update

--- starter/pages/cabins-page.js
+++ starter/pages/cabins-page.js
@@ -1,8 +1,8 @@
-import CabinCard from "@/app/_components/CabinCard";
+import CabinCard from "@/app/_components/CabinCard"
 
 export default function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = []
 
   return (
     <div>
@@ -13,9 +13,9 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature's beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
 
       {cabins.length > 0 && (
@@ -26,5 +26,5 @@ export default function Page() {
         </div>
       )}
     </div>
-  );
+  )
 }
