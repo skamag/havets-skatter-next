7e3808127b3963436021b376fdfd20ac321b68a8
unknown <magneskaro@gmail.com>
2024-08-26 13:58:38 +0200

    cabinCard debug

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import { UsersIcon } from "@heroicons/react/24/solid"
 
 function CabinCard({ cabin }) {
@@ -6,11 +7,14 @@ function CabinCard({ cabin }) {
 
   return (
     <div className="flex border-primary-800 border">
-      <Image
-        src={image}
-        alt={`Cabin ${name}`}
-        className="flex-1 border-r border-primary-800"
-      />
+      <div className="flex-1 relative">
+        <Image
+          src={image}
+          fill
+          alt={`Cabin ${name}`}
+          className="object-cover border-r border-primary-800"
+        />
+      </div>
 
       <div className="flex-grow">
         <div className="pt-5 pb-4 px-7 bg-primary-950">
@@ -43,12 +47,12 @@ function CabinCard({ cabin }) {
         </div>
 
         <div className="bg-primary-950 border-t border-t-primary-800 text-right">
-          <a
+          <Link
             href={`/cabins/${id}`}
             className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
           >
             Details & reservation &rarr;
-          </a>
+          </Link>
         </div>
       </div>
     </div>
