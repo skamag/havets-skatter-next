07404a3179d51324efc776b2c3f05902223b9686
unknown <magneskaro@gmail.com>
2024-09-02 15:14:51 +0200

    update

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,8 @@ import { Suspense } from "react"
 import Spinner from "../_components/Spinner"
 import CabinList from "../_components/CabinList"
 
+export const revalidate = 3600
+
 export const metadata = {
   title: "Cabins",
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -10,6 +11,7 @@ const nextConfig = {
       },
     ],
   },
+  // output: "export",
 }
 
 export default nextConfig
