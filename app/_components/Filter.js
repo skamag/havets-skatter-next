98233fefa5edd10c258774af4fc30456c140cbb9
unknown <magneskaro@gmail.com>
2024-09-12 15:25:42 +0200

    update

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -7,6 +7,8 @@ function Filter() {
   const router = useRouter()
   const pathname = usePathname()
 
+  const activeFilter = searchParams.get("capacity" ?? "all")
+
   function handleFilter(filter) {
     const params = new URLSearchParams(searchParams)
     params.set("capacity", filter)
@@ -15,7 +17,7 @@ function Filter() {
 
   return (
     <div className="border border-primary-800 flex">
-      <button
+      {/* <button
         className="px-5 py-2 hover:bg-primary-700"
         onClick={() => handleFilter("all")}
       >
@@ -38,8 +40,50 @@ function Filter() {
         onClick={() => handleFilter("large")}
       >
         8&mdash;12 guests
-      </button>
+      </button> */}
+
+      <Button
+        filter="all"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        All cabins
+      </Button>
+      <Button
+        filter="small"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        2&mdash;3 guests
+      </Button>
+      <Button
+        filter="medium"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        4&mdash;7 guests
+      </Button>
+      <Button
+        filter="large"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        8&mdash;12 guests
+      </Button>
     </div>
+  )
+}
+
+function Button({ filter, handleFilter, activeFilter, children }) {
+  return (
+    <button
+      className={`px-5 py-2 hover:bg-primary-700 ${
+        filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
+      }`}
+      onClick={() => handleFilter(filter)}
+    >
+      {children}
+    </button>
   )
 }
 
