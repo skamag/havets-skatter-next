392c84dc1524f77cc16a21f5f48b84756835e1be
unknown <magneskaro@gmail.com>
2024-09-13 13:57:18 +0200

    update

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -4,6 +4,7 @@ import { isWithinInterval } from "date-fns"
 import { useState } from "react"
 import { DayPicker } from "react-day-picker"
 import "react-day-picker/dist/style.css"
+import { useReservation } from "./ReservationContext"
 
 function isAlreadyBooked(range, datesArr) {
   return (
@@ -16,7 +17,7 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ settings, cabin, bookedDates }) {
-  const [range, setRange] = useState({ from: undefined, to: undefined })
+  const { range, setRange, resetRange } = useReservation()
 
   // CHANGE
   const regularPrice = 23
@@ -74,7 +75,7 @@ function DateSelector({ settings, cabin, bookedDates }) {
         {range.from || range.to ? (
           <button
             className="border border-primary-800 py-2 px-4 text-sm font-semibold"
-            onClick={() => resetRange()}
+            onClick={resetRange}
           >
             Clear
           </button>
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import { Suspense } from "react"
 import Spinner from "../_components/Spinner"
 import CabinList from "../_components/CabinList"
 import Filter from "../_components/Filter"
+import ReservationReminder from "../_components/ReservationReminder"
 
 export const revalidate = 3600
 
@@ -31,6 +32,7 @@ export default async function Page({ searchParams }) {
       </div>
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        {/* <ReservationReminder /> */}
       </Suspense>
     </div>
   )
