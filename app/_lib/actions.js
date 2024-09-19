f547c7dfa00fd85b6363cc609fc6cf80d113e8ce
unknown <Fundri@UL04.funkweb.lan>
2024-09-19 12:12:31 +0200

    update

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -28,8 +28,33 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile")
 }
 
-export async function deleteReservation(bookingId) {
-  await new Promise((res) => setTimeout(res, 2000))
+export async function createBooking(bookingData, formData) {
+  const session = await auth()
+  if (!session) throw new Error("You must be logged in")
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPain: false,
+    hasBreakfast: false,
+    status: "unconfirmed",
+  }
+
+  const { error } = await supabase.from("bookings").insert([newBooking])
+
+  if (error) throw new Error("Booking could not be created")
+
+  revalidatePath(`/cabins/${bookingData.cabinId}`)
+
+  redirect("/cabins/thankyou")
+}
+
+export async function deleteBooking(bookingId) {
+  // await new Promise((res) => setTimeout(res, 2000))
 
   const session = await auth()
   if (!session) throw new Error("You must be logged in")
